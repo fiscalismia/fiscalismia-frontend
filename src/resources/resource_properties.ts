597559139bab6e4dee0f4defd90d497cc22e9597
hangrybear666 <hangrybear666+github@proton.me>
2026-02-28 14:21:23 +0100

    separates routes for backend api and fastapi

--- src/resources/resource_properties.ts
+++ src/resources/resource_properties.ts
@@ -183,5 +183,5 @@ export const investmentInputCategories = {
 
 export const serverConfig = {
   API_BASE_URL: `${__BACKEND_PROTOCOL__}://${__BACKEND_DOMAIN__}:${__BACKEND_PORT__}/api/fiscalismia`,
-  FASTAPI_BASE_URL: `${__BACKEND_PROTOCOL__}://${__BACKEND_DOMAIN__}:${__FASTAPI_PORT__}/api/fiscalismia`
+  FASTAPI_BASE_URL: `${__BACKEND_PROTOCOL__}://${__BACKEND_DOMAIN__}:${__FASTAPI_PORT__}/fastapi/fiscalismia`
 };
